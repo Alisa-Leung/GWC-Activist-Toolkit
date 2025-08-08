14537267d1996c39acde6ff6adff2c43183ea623
Alisa Leung <alisal.singyee@gmail.com>
2025-08-08 14:13:53 +0000

    Updating Files

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-/* .js files add interaction to your website */
+var factList = [
+  "avoid sharing personally identifying information (PII) with people online, even those I know",
+  "fact-check before I believe what I hear/see online",
+  "avoid clicking suspicious links from untrusted or unknown sources",
+  "be mindful of what I choose to share or post online",
+  "use strong passwords to protect my accounts",
+  "approach online interactions with caution",
+  "use privacy settings on social media accounts to protect my information",
+  "make sure I'm only downloading files from trusted sites",
+  "use 2-factor authentication to protect my online accounts",
+  "report suspicious activity to appropriate authorities or trusted adults"
+];
 
+var button = document.getElementById("factButton");
+var fact = document.getElementById("factText");
+var count = 0
+
+button.addEventListener("click", generateFact);
+
+function generateFact(){
+  fact.innerHTML = factList[count];
+  count += 1;
+  if (count == factList.length){
+    count = 0;
+  }
+}
